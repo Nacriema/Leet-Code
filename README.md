3a737f8549a2dc913759773c488d604fdf061a3b
Nacriema <huytruong2931998@gmail.com>
2024-01-02 00:09:05 +0700

    Add solution for the Redistribute Characters to Make All Strings Equal problem

--- README.md
+++ README.md
@@ -123,7 +123,8 @@ Example: `reorganize-string.py` will solve the problem stated at `https://leetco
 - [x] `24/12/2023` Add solution for the `Minimum Changes To Make Alternating Binary String` problem
 - [x] `25/12/2023` Add solution for the `Decode Ways` problem
 - [x] `26/12/2023` Add solution for the `Minimum Time to Make Rope Colorful`, `Minimum Operations to Make the Array Increasing` problem
-- [x] `01/01/2024` Add solution for the `Assign Cookies`, `Largest Substring Between Two Equal Characters` problem
+- [x] `01/01/2024` Add solution for the `Assign Cookies`, `Largest Substring Between Two Equal Characters`, 
+`Redistribute Characters to Make All Strings Equal` problem
 
 ## Review solution
 
---
+++ daily_challenges/redistribute-characters-to-make-all-strings-equal.py
@@ -0,0 +1,47 @@
+"""
+    Problem statement:
+        You are given an array of strings words (0-indexed).
+        In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, 
+        and move any character from words[i] to any position in words[j].
+        Return true if you can make every string in words equal using any number of operations, 
+        and false otherwise.
+
+        Example 1:
+            Input: words = ["abc","aabc","bc"]
+            Output: true
+            Explanation: 
+                Move the first 'a' in words[1] to the front of words[2],
+                to make words[1] = "abc" and words[2] = "abc".
+                All the strings are now equal to "abc", so return true.
+
+        Example 2:
+            Input: words = ["ab","a"]
+            Output: false
+            Explanation: It is impossible to make all the strings equal using the operation.
+
+        Constraints:
+            * 1 <= words.length <= 100
+            * 1 <= words[i].length <= 100
+            * words[i] consists of lowercase English letters.
+"""
+from typing import List
+from collections import Counter
+from itertools import groupby
+
+
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        counter = Counter(''.join(words))
+        for val in list(counter.values()):
+            if val % len(words) != 0:
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.makeEqual(words = ["abc","aabc","bc"]))
+    print(s.makeEqual(words = ["ab","a"]))
+    print(s.makeEqual(words = ["a", "b"]))
+    print(s.makeEqual(words = ["b"]))
+    print(s.makeEqual(words = ["caaaaa","aaaaaaaaa","a","bbb","bbbbbbbbb","bbb","cc","cccccccccccc","ccccccc","ccccccc","cc","cccc","c","cccccccc","c"]))
